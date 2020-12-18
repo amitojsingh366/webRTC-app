410b1c557d3f98a3ecac12dfe13aec4a6cd31d92
Amitoj Singh <amitojsingh366@gmail.com>
2020-12-19 01:49:52 +0300

    meoeooe

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const { v4: uuidV4 } = require('uuid');
 
+const port = process.env.port || 3000;
+
 app.set('view engine', 'ejs');
 app.use(express.static('static'));
 
